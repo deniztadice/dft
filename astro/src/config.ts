2a023c4626ba1efa5b84ac8fb574c1b47569c100
DenizKose <deniz8011@gmail.com>
2024-05-05 17:33:06 +0300

    Updated astro

--- astro/src/config.ts
+++ astro/src/config.ts
@@ -1,4 +1,4 @@
-export const MODULE_LINK = 'https://raw.githubusercontent.com/deniztadice/dft/main/module.json'
+export const MODULE_LINK = 'https://github.com/deniztadice/dft/releases/latest/download/module.json'
 export const NAVBAR_MENU = [
     ["pages.index", '/'],
     ["pages.journal", '/journal', [["pages.journal_showcase", "/journal/showcase"],["pages.journal_examples", "/journal/examples"]]],
